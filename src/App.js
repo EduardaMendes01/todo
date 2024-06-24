4a81c6643d23adc81189756c8dab98de901479c7
EduardaMendes01 <eduardamendes0100@gmail.com>
2024-06-24 20:09:01 -0300

    refactoring and finalization

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
 import React, {useState} from 'react';
-import { MdDelete } from 'react-icons/md';
 
-import './App.css';
+import NewTodo from './components/NewTodo';
+import TodoList from './components/TodoList';
+
+
 
 const App = () => {
 
-    const ESCAPE_KEY = 27;
-    const ENTER_KEY = 13;
-
     const [todos, setTodos] = useState([]);
-    const [value, setValue] = useState('');
 
-    const erase = () => {
-        setValue('');
-    }
-
-    const submit = () => {
-           setTodos([
+    const onNewTodo = (value) => {
+        setTodos([
             ...todos,
             {
             // Dont use in bigger systems, need liberies, for secury too and random numbers maybe can repeat too
             id: new Date().getTime() ,
-            title: value ,
+            title: value,
             checked: false,
         },
     ]);
-
-        erase();
     }
-
-    const onChange = (event) => {
-        setValue(event.target.value);
-    }
-
-    const onKeyDown = (event) =>{
-        if(event.which === ENTER_KEY){
-            submit();
-        }else if (event.which === ESCAPE_KEY){
-            erase();
-        }
-    }
-
     const onToggle = (todo) => {
         setTodos(
             todos.map((obj) =>
@@ -61,34 +40,9 @@ return(
             <h1 className='title'>Todo</h1>
         </header>
         <section className='main'>
-            <input
-            className='new-todo'
-            placeholder='What needs to be done?' value={value}
-            onChange={onChange}
-            onKeyDown={onKeyDown}
-            />
-            <ul className='todo-list'>
-                {
-                    todos.map((todo) => (
-                        <li key={todo.id.toString()}>
-                            <span
-                            className={['todo', todo.checked ? 'checked' : ''].join(' ')}
-                            onClick={() => onToggle(todo)}
-                            onKeyPress={() => onToggle(todo)}
-                            role='button'
-                            tabIndex={0}
-                            >
-                            {todo.title}</span>
-                            <button className='remove' type='button'
-                            onClick={() => onRemove(todo)}
-                            >
-                                <MdDelete size={28} />
-                            </button>
+            <NewTodo onNewTodo={onNewTodo}/>
+            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove}/>
 
-                        </li>
-                    ))
-                }
-            </ul>
         </section>
     </section>
 );
